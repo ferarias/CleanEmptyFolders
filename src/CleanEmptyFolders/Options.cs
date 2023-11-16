0a994dea954804b605fde08d81df3952d6b3c4dc
Fernando Arias <fernando.arias@sembo.se>
2023-11-16 15:25:26 +0100

    20231116

--- src/CleanEmptyFolders/Options.cs
+++ src/CleanEmptyFolders/Options.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CommandLine;
+using CommandLine.Text;
 
 namespace CleanEmptyFolders
 {
 	public class Options
 	{
-		[Option('f', "folder", Required = true,	HelpText = "Starting folder.")]
-		public string StartingFolder { get; set; } 
+		[Option('f', "folder", Required = true, HelpText = "Starting folder.")]
+		public string StartingFolder { get; set; }
 
 		[Option('x', "execute", Required = false, Default = false, HelpText = "Actually execute changes. Otherwise, just simulate.")]
 		public bool Execute { get; set; }
@@ -16,5 +16,16 @@ namespace CleanEmptyFolders
 		[Option(HelpText = "Prints all messages to standard output.")]
 		public bool Verbose { get; set; }
 
+		[Usage(ApplicationAlias = "CleanEmptyFolders")]
+		public static IEnumerable<Example> Examples
+		{
+			get
+			{
+				return new List<Example>()
+				{
+					new Example("Check which empty folders will be deleted (dry run)", new Options { StartingFolder = "D:\\photo", Execute = false })
+				};
+			}
+		}
 	}
 }
--- src/CleanEmptyFolders/Program.cs
+++ src/CleanEmptyFolders/Program.cs
@@ -6,41 +6,29 @@ using CommandLine;
 
 namespace CleanEmptyFolders
 {
-	class MainClass
+	static class MainClass
 	{
-		private static List<string> emptyDirs = new List<string>();
+		private static List<string> emptyDirs = [];
 
 		public static int Main(string[] args)
 		{
 			Console.WriteLine("Clean Empty folders");
 
 			var result = Parser.Default.ParseArguments<Options>(args);
-			var exitCode = result.MapResult(options =>
-											DoIt(options), errors =>
-											{
-
-												Console.WriteLine(errors);
-												return 1;
-											});
+			var exitCode = result.MapResult(DoIt, errors => 
+													{
+														Console.WriteLine(errors);
+														return 1;
+													});
 			Console.WriteLine("Clean Empty folders finished with exit code " + exitCode);
 			return exitCode;
 		}
 
 		static int DoIt(Options options)
 		{
-			if (options.Verbose) Console.WriteLine("Starting folder: {0}", options.StartingFolder);
 
-			if (!Directory.Exists(options.StartingFolder))
-			{
-				Console.WriteLine("Folder '{0}' does not exist.", options.StartingFolder);
-				return 1;
-			}
-			else
-			{
-				Console.WriteLine("Starting in '{0}'.", options.StartingFolder);
-			}
 
-			emptyDirs = new List<string>();
+			emptyDirs = [];
 			ProcessDir(options.StartingFolder);
 
 			foreach (var item in emptyDirs)
@@ -71,18 +59,14 @@ namespace CleanEmptyFolders
 
 			var files = Directory.GetFiles(dir);
 
-			if (dirs.Length == 0 && (FilesAreUnimportant(files)))
+			if (dirs.Length == 0 && FilesAreUnimportant(files))
 				emptyDirs.Add(dir);
 
 		}
 
-		private static bool FilesAreUnimportant(string[] files)
-		{
-			return !files.Any() || files.Any() && files.All(FileIsUnimportant);
+        private static bool FilesAreUnimportant(string[] files) => files.Length == 0 || files.Length != 0 && files.All(FileIsUnimportant);
 
-		}
-
-		static bool FileIsUnimportant(string item)
+        static bool FileIsUnimportant(string item)
 		{
 			if (item.EndsWith(".AAE", StringComparison.InvariantCultureIgnoreCase))
 				return true;
